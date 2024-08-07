217650397ae2433d8a7a1ecfcc2093df60ffc470
unknown <kalyaninalamaru.com>
2024-08-07 14:10:24 +0530

    terraform

--- loops/count/output.tf
+++ loops/count/output.tf
@@ -2,17 +2,17 @@ output "instance_info" {
     value =aws_instance.web
 }
 
-##it gives ipaddress
-output "instance_id" {
-    value= aws_instance.web.id
-}
+# ##it gives ipaddress
+# output "instance_id" {
+#     value= aws_instance.web.id
+# }
 
-##it gives Public IP
-output "public_ip" {
-    value =aws_instance.web.public_ip
-}
+# ##it gives Public IP
+# output "public_ip" {
+#     value =aws_instance.web.public_ip
+# }
 
-##it gives Private IP
-output "private_ip" {
-    value =aws_instance.web.private_ip
-}
+# ##it gives Private IP
+# output "private_ip" {
+#     value =aws_instance.web.private_ip
+# }
