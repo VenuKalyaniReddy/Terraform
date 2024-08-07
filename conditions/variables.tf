aa359e68f2dd0fa843cd2ca8ec5977c255bec618
unknown <kalyaninalamaru.com>
2024-08-07 13:58:21 +0530

    terraform

---
+++ conditions/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_name" {
+    type = string
+    default = "mongoDB"
+}
+
+
+variable "ami_id" {
+    type = string
+    default = "ami-041e2ea9402c46c32"  ### devops-practice RHEL
+}
+
