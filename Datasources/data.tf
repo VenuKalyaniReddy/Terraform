3331ce301f674ddf2dda6384667997b5f554c0ab
unknown <kalyaninalamaru.com>
2024-08-09 14:35:04 +0530

    terraform

--- Datasources/data.tf
+++ Datasources/data.tf
@@ -18,4 +18,27 @@ data "aws_ami" "RHEL-9"{
     }
 }
 
+data "aws_ami" "aws-linux-2"{
+    owners = ["137112412989"]
+    most_recent      = true
+
+    filter {
+        name   = "name"
+        values = ["al2023-ami-2023.5.20240805.0-kernel-*"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
 
